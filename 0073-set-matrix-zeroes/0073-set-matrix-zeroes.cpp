64c5cebb28964e5d12f873eafaceeab5dcc53b86
ittisha17 <ittishaharpavat17@gmail.com>
2025-12-29 20:33:20 +0530

    Time: 3 ms (10.67%), Space: 18.8 MB (22.15%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        set<int> rows;
+        set<int> cols;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==0)
+                 {
+                    rows.insert(i);
+                    cols.insert(j);
+                 }
+            }
+        }
+
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(rows.find(i)!=rows.end()  || cols.find(j)!=cols.end())
+                   matrix[i][j]=0;
+            }
+        }
+
+    }
+};
