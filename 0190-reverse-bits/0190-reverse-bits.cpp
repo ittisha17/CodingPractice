9f175e58f6549d24c0cd35c4a35a41c005f3d108
ittisha17 <ittishaharpavat17@gmail.com>
2026-01-07 18:12:10 +0530

    Time: 0 ms (100%), Space: 8.3 MB (43.64%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans=0;
+
+        for(int i=0;i<32;i++)
+        {
+            if(n&1)
+             ans+=(1<<31-i);
+            n=n>>1;
+        }
+       
+        return ans;
+    }
+};
