e4de50cab421bf2560d91269ce45b2e29dc25f81
ittisha17 <ittishaharpavat17@gmail.com>
2026-01-26 10:49:57 +0530

    Time: 1 ms (83.86%), Space: 14.4 MB (26.09%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -6,25 +6,23 @@ public:
         if (numRows == 1 || numRows >= s.length())
             return s;
 
-        int n = s.length();
-        int T = 2 * numRows - 2;   // total cycle length
-        string result = "";
-
-        // Traverse row by row
-        for (int i = 0; i < numRows; i++) {
-            for (int j = i; j < n; j += T) {
-                // Vertical character
-                result += s[j];
-
-                // Diagonal character (only for middle rows)
-                int diag = j + T - 2 * i;
-                if (i != 0 && i != numRows - 1 && diag < n) {
-                    result += s[diag];
-                }
-            }
+       vector<string> arr(numRows,"");
+       int direction=1;
+       int index=0;
+       string res="";
+       for(auto ch:s)
+       {   
+          arr[index]+=ch;
+           if(index==numRows-1) //last row
+            direction=-1;  //move upwards
+           else if(index==0)
+            direction=1;
+          index+=direction;  //whihc row to add character next
         }
-        return result;
-    }
+        for(auto s:arr)
+         res+=s;
+        return res;
+}
 };
 
        
