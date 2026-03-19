395656ab29d487e25df9e41a7d96420e6b4e37b1
ittisha17 <ittishaharpavat17@gmail.com>
2026-03-19 16:57:21 +0530

    Time: 3 ms (69.76%), Space: 13.1 MB (82.59%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,34 @@
+
+
+void solve(int open,int closed,vector<string>&res,string &curr,int n)
+{  
+    if(closed>open)
+     return;
+    if(curr.length()==2*n)
+     res.push_back(curr);
+    if(open<n)
+     {
+        curr+="(";
+        solve(open+1,closed,res,curr,n);
+        curr.pop_back();
+     }
+    if(open>closed)
+    {
+        curr+=")";
+        solve(open,closed+1,res,curr,n);
+        curr.pop_back();
+    }
+     
+}
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        int open=0;
+        int closed=0;
+        vector<string> res;
+        string curr="";
+        solve(open,closed,res,curr,n);
+        return res;
+    }
+};
