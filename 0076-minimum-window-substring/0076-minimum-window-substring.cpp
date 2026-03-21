95a286c99552ed6736334d35317f8c485a542c76
ittisha17 <ittishaharpavat17@gmail.com>
2026-03-21 11:09:42 +0530

    Time: 44 ms (11.3%), Space: 12.3 MB (5.62%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,51 +1,58 @@
+
+
+bool valid(vector<int>&curr_freq,vector<int>&req_freq,set<char>&st)
+{
+    for(auto x:st)
+    {
+        if(curr_freq[x]<req_freq[x])
+         return false;
+    }
+    return true;
+}
+
 class Solution {
 public:
     string minWindow(string s, string t) {
         int m=s.length();
         int n=t.length();
+        vector<int> curr_freq(256,0);
+        vector<int> req_freq(256,0);
+        set<char> st;
+        for(int i=0;i<n;i++)
+        {
+            req_freq[t[i]]++;
+            st.insert(t[i]);
+        }
+        int ln=INT_MAX;
         int start=0;
         int i=0;
-        int j=0;
-        int upd_st;
-        int ln=INT_MAX;
-        unordered_map<char,int> mp;
-        unordered_map<char,int> org_mp;
-        for(int i=0;i<n;i++)
-          {mp[t[i]]++;
-           org_mp[t[i]]++;
-          }
-
+        set<char> matched;
+        int idx=-1;
         while(i<m)
         {   
-
-            if(mp.find(s[i])!=mp.end()) //character of t
-            {
-                if(mp[s[i]]>0)
-                  j++;
-                mp[s[i]]--;
-            }
-            while(j==n)
-            {   
-                int curr_len=i-start+1;
-                if(curr_len<ln)
-                {
-                    ln=curr_len;
-                    upd_st=start;
-                }
-                if(mp.find(s[start])!=mp.end())
-                {
-                    mp[s[start]]++;
-                    if(mp[s[start]]>0)
-                     j--;
-                } //shrink window
-                start++;
+            curr_freq[s[i]]++;
+            if(st.find(s[i])!=st.end()) //character of t
+             matched.insert(s[i]);
+            while(matched.size() == st.size() && valid(curr_freq,req_freq,st) )  //all characters found
+            {       
+                    int cur_len=i-start+1;
+                    if(cur_len<ln)
+                    {
+                        ln=cur_len;
+                        idx=start;
+                    }
+                    curr_freq[s[start]]--;
+                    if(curr_freq[s[start]]==0 && st.find(s[start])!=st.end()) //character of t and not  anymore in curr window
+                    matched.erase(s[start]);
+                    //ch_matched--;
+                    start++;
             }
             i++;
         }
-        if(ln==INT_MAX)
+        if(idx==-1)
          return "";
         else
-         return(s.substr(upd_st,ln));
-
+         return s.substr(idx,ln);
+        
     }
 };
