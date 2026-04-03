49527348cede31a76f203cee6287fd8a2684a20b
ittisha17 <ittishaharpavat17@gmail.com>
2026-04-03 15:21:55 +0530

    Time: 7 ms (27.91%), Space: 26.9 MB (97.33%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -9,33 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-TreeNode* construct(int&id,int l,int h,int n,vector<int>&inorder,vector<int>&postorder)
+TreeNode* Build(int st,int end,int &idx,vector<int>&inorder,vector<int>&postorder)
 {
-    if(l>h) return NULL;
-    int rt_val=postorder[id--];
-    TreeNode*root=new TreeNode(rt_val);
-    int rt_id;
-    for(int i=l;i<=h;i++)
-    {  if(inorder[i]==rt_val)
-        { rt_id=i;
-         break;
-        }
+    if(st>end)
+     return NULL;
+    int val=postorder[idx];
+    int inord_id=-1;
+    for(int i=st;i<=end;i++)
+    {
+        if(inorder[i]==val)
+         {
+            inord_id=i;
+            break;
+         }
     }
+    TreeNode* root=new TreeNode(val);
+    idx--;
+    root->right=Build(inord_id+1,end,idx,inorder,postorder);
+    root->left=Build(st,inord_id-1,idx,inorder,postorder);
 
-    
-    root->right=construct(id,rt_id+1,h,n,inorder,postorder);
-    root->left=construct(id,l,rt_id-1,n,inorder,postorder);
-    return root;
+    return root;  
 }
 
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n=inorder.size();
-        int h=n-1;
-        int l=0;
-        int pst_id=h;
-        TreeNode* root=construct(pst_id,l,h,n,inorder,postorder);
-        return root;
-}
+        int st=0;
+        int end=n-1;
+        int id=n-1;
+        return Build(st,end,id,inorder,postorder);
+    }
 };
