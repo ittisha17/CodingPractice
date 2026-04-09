ebc9b675b9239902421bd850eba96ba73cb55b80
ittisha17 <ittishaharpavat17@gmail.com>
2026-04-09 16:57:03 +0530

    Time: 61 ms (7.51%), Space: 24.8 MB (9.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,32 +1,35 @@
+
 void dfs(int x,int y,vector<vector<bool>>&visited,vector<vector<char>>&grid,int n,int m)
-{   
+{
     visited[x][y]=true;
-    vector<int> dx={-1,1,0,0};
-    vector<int>dy={0,0,1,-1};
+    vector<int>dx={0,0,1,-1};
+    vector<int>dy={1,-1,0,0};
     for(int i=0;i<4;i++)
     {
         int nx=x+dx[i];
         int ny=y+dy[i];
-        if(nx>=0 && ny>=0 && nx<n && ny<m && !visited[nx][ny] && grid[nx][ny]=='1')
-           dfs(nx,ny,visited,grid,n,m);
+        if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && grid[nx][ny]=='1')
+         dfs(nx,ny,visited,grid,n,m);
     }
-    return;
 }
+
 
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int cnt=0;
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<bool>> visited(n,vector<bool>(m,false));
+        int cnt=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(!visited[i][j] && grid[i][j]=='1')
-                 { dfs(i,j,visited,grid,n,m);
-                  cnt++;}
+                if(!visited[i][j] && grid[i][j]=='1')//possible island
+                {
+                    dfs(i,j,visited,grid,n,m);
+                    cnt++;
+                }
             }
         }
         return cnt;
