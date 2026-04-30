b405aca7b35f4e422341da6e0e8348a061554f36
ittisha17 <ittishaharpavat17@gmail.com>
2026-04-30 18:35:26 +0530

    Time: 11 ms (18.97%), Space: 26.9 MB (97.44%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,35 +9,34 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-TreeNode* construct(int &id,int l,int h,vector<int>&preorder,vector<int>&inorder,int n)
+TreeNode* Construct (int &idx,int st,int end,vector<int>&preorder,vector<int>&inorder)
 {
-    if(l>h) return NULL;
-    int rt_val=preorder[id];
-    id++;
-    int rt_id;
-    for(int i=l;i<=h;i++)
-     {
-        if(inorder[i]==rt_val)
-         {
-            rt_id=i;
+    if(st>end) return NULL;
+   
+    TreeNode*root=new TreeNode(preorder[idx]);
+    int value=root->val;
+    int index=-1;
+    for(int i=st;i<=end;i++)
+    {
+        if(value==inorder[i])
+        {
+            index=i;
             break;
-         }
-     }
-     TreeNode*root=new TreeNode(rt_val);
-     root->left=construct(id,l,rt_id-1,preorder,inorder,n);
-     root->right=construct(id,rt_id+1,h,preorder,inorder,n);
-     return root;
+        }
+    }
+    idx++;
+    root->left=Construct(idx,st,index-1,preorder,inorder);
+    root->right=Construct(idx,index+1,end,preorder,inorder);
+
+    return root;
 }
 
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int pre_idx=0;
-        TreeNode*root;
-        int l=0;
-        int n=preorder.size();
-        int h=preorder.size()-1;
-        root=construct(pre_idx,l,h,preorder,inorder,n);
-        return root;
+        int idx=0;
+        int st=0;
+        int end=inorder.size()-1;
+        return Construct(idx,st,end,preorder,inorder);
     }
 };
