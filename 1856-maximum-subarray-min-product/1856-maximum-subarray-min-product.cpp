851913248d8a4f330b840bd21ff28780f13b3b91
ittisha17 <ittishaharpavat17@gmail.com>
2026-04-30 16:24:06 +0530

    Time: 43 ms (40.61%), Space: 94.4 MB (67.1%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.cpp
@@ -1,62 +1,69 @@
+void prev_sm(vector<int>&nums,vector<int>&ps)
+{
+    stack<int> st;
+    int n=nums.size();
+    st.push(0);
+    ps[0]=-1;
+    for(int i=1;i<n;i++)
+    {
+      while(!st.empty() && nums[i]<=nums[st.top()])
+       st.pop();
+      if(st.empty())
+       ps[i]=-1;
+      else
+       ps[i]=st.top();
+      st.push(i);
+    }
+}
+
+void next_sm(vector<int>&nums,vector<int>&ns)
+{
+    stack<int> st;
+    int n=nums.size();
+    st.push(n-1);
+    ns[n-1]=n;
+    for(int i=n-2;i>=0;i--)
+    {
+      while(!st.empty() && nums[i]<=nums[st.top()])
+       st.pop();
+      if(st.empty())
+       ns[i]=n;
+      else
+       ns[i]=st.top();
+      st.push(i);
+    }
+}
+
+
 class Solution {
 public:
     int maxSumMinProduct(vector<int>& nums) {
+        int mod=1e9+7;
         int n=nums.size();
-        vector<int> ps(n);
-        vector<int> ns(n);
-        ps[0]=-1;
-        ns[n-1]=n;
-        vector<long long> p_sum(n);
-        p_sum[0]=nums[0];
-        for(int i=1;i<n;i++)
-         p_sum[i]=nums[i]+p_sum[i-1];
-        
-        stack<int> st1;
-        st1.push(0);
-        for(int i=1;i<n;i++)
+        vector<long long> pre_sum(n);
+        vector<int> pre_sm(n);
+        vector<int> nex_sm(n);
+
+        prev_sm(nums,pre_sm);
+        next_sm(nums,nex_sm);
+
+        pre_sum[0]=nums[0];
+        for(int i=1;i<nums.size();i++)
         {
-            while(!st1.empty() && nums[st1.top()]>nums[i])
-             st1.pop();
-            if(st1.empty())
-             ps[i]=-1;
-            else
-             ps[i]=st1.top();
-            
-            st1.push(i);
+            pre_sum[i]=nums[i]+pre_sum[i-1];
         }
-        stack<int>st2;
-        st2.push(n-1);
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st2.empty() && nums[st2.top()]>=nums[i])
-             st2.pop();
-            if(st2.empty())
-             ns[i]=n;
-            else
-             ns[i]=st2.top();
-            
-            st2.push(i);
-        }
+
 
         long long ans=0;
-        int mod=1e9+7;
-        for(int i=0;i<n;i++)
-        {   
-            int l=ps[i]+1;
-            int r=ns[i]-1;
-           long long total;
-          if(l == 0)
-            total = p_sum[r];
-          else
-           total = p_sum[r] - p_sum[l-1];
-            long long val=(nums[i]*total);
-            ans=max(ans,val);
+        for(int i=0;i<n;i++)  //calculating max min prod for every elemnt
+        {
+           long long  elem=nums[i];
+           int r=nex_sm[i]-1;
+           int l=pre_sm[i]+1;
+           long long  sum=pre_sum[r]- (l>0? pre_sum[l-1]:0);
+           long long val=(elem*sum);
+           ans=max(ans,val);
         }
         return ans%mod;
-
-        
-
-        
-        
     }
 };
