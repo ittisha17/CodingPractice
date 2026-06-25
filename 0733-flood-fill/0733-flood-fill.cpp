4f67b457644da4fcda3293b38b98a16834b6fc92
ittisha17 <ittishaharpavat17@gmail.com>
2026-06-25 14:56:43 +0530

    Time: 0 ms (100%), Space: 18.3 MB (32.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,36 @@
 
-void dfs(int x,int y,int n,int m,int org,int color,vector<vector<bool>>&visited,vector<vector<int>>&image)
+void dfs(int x,int y,vector<vector<bool>>&vis,int n,int m,vector<vector<int>>&img)
 {
-    visited[x][y]=true;
-    image[x][y]=color;
+    vis[x][y]=true;
     vector<int> dx={0,0,1,-1};
     vector<int> dy={1,-1,0,0};
     for(int i=0;i<4;i++)
     {
-        int nx=x+dx[i];
-        int ny=y+dy[i];
-        if(nx>=0 && nx<n && ny>=0 && ny<m && image[nx][ny]==org && !visited[nx][ny])
-         dfs(nx,ny,n,m,org,color,visited,image);
+       int nx=x+dx[i];
+       int ny=y+dy[i];
+       if(nx>=0 && nx<n && ny>=0 && ny<m && !vis[nx][ny] && img[nx][ny]==img[x][y] )
+       {
+         dfs(nx,ny,vis,n,m,img);
+       }
     }
 }
-
-
 
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
-        //basically starting from the inital point where all can i reach such that it has the same value value as the initial node;
         int n=image.size();
         int m=image[0].size();
-        int org=image[sr][sc];
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
-        dfs(sr,sc,n,m,org,color,visited,image);
-        return image;
-
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        dfs(sr,sc,vis,n,m,image);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(vis[i][j])
+                 image[i][j]=color;
+            }
+        }
+    return image;
+        
     }
 };
