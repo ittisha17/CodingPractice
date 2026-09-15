4c48b9f12d5f9aff55206c100c2d97ec56eb7a4d
ittisha17 <ittishaharpavat17@gmail.com>
2026-09-15 14:42:19 +0530

    Time: 54 ms (22.18%), Space: 43.2 MB (24.99%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,85 +1,78 @@
-class DSU{
-    public:
-    int n;
-    vector<int> parent;
-    vector<int> rank;
-
-    DSU(int n)
-    {
-        this->n=n;
-        for(int i=0;i<n;i++)
-        { parent.push_back(i);
-          rank.push_back(0);
+void combine_res(set<int>&comp,vector<vector<string>>&res,vector<vector<string>>& accounts)
+{
+   vector<string>temp;
+   string name="";
+   set<string> emails;
+    for(auto id:comp)
+    {   
+        if(name=="")
+          {name=accounts[id][0];
+           temp.push_back(name);
+          }
+        for(int i=1;i<accounts[id].size();i++)
+        {
+            emails.insert(accounts[id][i]);
         }
+           
+    }
+    for(auto e:emails)
+    {
+        temp.push_back(e);
+    }
+    sort(temp.begin()+1,temp.end());
+    res.push_back(temp);
+}
+
+void dfs(int u,vector<bool>&visited,vector<vector<int>>&adj,set<int>&comp)
+{
+    visited[u]=true;
+    comp.insert(u);
+    for(int v:adj[u])
+    {
+        if(!visited[v]) 
+         dfs(v,visited,adj,comp);
     }
      
-    int find(int x)
-    {
-        if(parent[x]==x)
-         return x;
-        return parent[x]=find(parent[x]);
-    }
-    void unite(int u,int v)
-    {   
-        int pu=find(u);
-        int pv=find(v);
-        if(pu==pv)
-         return;
-        
-        if(rank[pu]==rank[pv])
-        {
-            parent[pv]=pu;
-            rank[pu]++;
-        }
-        else if(rank[pu]>rank[pv])
-         parent[pv]=pu;
-        else
-         parent[pu]=pv;
-    }
-};
-
-
+}
 
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n=accounts.size();
-        DSU dsu(n);
+        vector<vector<string>> res;
+                int n=accounts.size();
 
+        vector<vector<int>> adj(n);
         unordered_map<string,int> mp;
-        for(int i=0;i<n;i++)
-        {;
+        for(int i=0;i<accounts.size();i++)
+        {
             for(int j=1;j<accounts[i].size();j++)
             {
-                if(mp.find(accounts[i][j])==mp.end())
-                 mp[accounts[i][j]]=i;
-                else
-                 dsu.unite(i,mp[accounts[i][j]]);   
+              string mail=accounts[i][j];
+              if(mp.find(mail)==mp.end())
+               {
+                 mp[mail]=i;
+               }
+              else //if mail already present
+              { int u=i;
+                int v=mp[mail];
+                adj[u].push_back(v);
+                adj[v].push_back(u);
+              }
             }
         }
-        
-        unordered_map<int,vector<string>> groups;
-        for(auto p:mp){
-            string mail=p.first;
-            int acnt=p.second;
-            int parent=dsu.find(acnt);
-            groups[parent].push_back(mail);
-        }
-       
-       vector<vector<string>> res;
-       for(auto it:groups)
-       {
-         int parent=it.first;
-         vector<string> mails=it.second;
-         sort(mails.begin(),mails.end());
-         vector<string> temp;
-         temp.push_back(accounts[parent][0]);
-         for(auto s:mails)
-          temp.push_back(s);
-         res.push_back(temp);
 
-       }
-       return res;
+        vector<bool>visited(n,false);
+        for(int i=0;i<n;i++)
+        {   
+            
+            if(!visited[i])
+            {   set<int> comp;
+                dfs(i,visited,adj,comp);
+                combine_res(comp,res,accounts);
+            }
+        }
+        return res;
+
 
     }
 };
